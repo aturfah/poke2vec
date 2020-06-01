0c690fb4ebb680eb9aa0c27fde488a9e566ce0f4
Ali Turfah <aturfah@gmail.com>
2020-05-31 23:07:46 -0400

    Dont output the graph

--- analysis.R
+++ analysis.R
@@ -14,12 +14,12 @@ rownames(avg_mat) <- name_vec
 mat_pcs <- prcomp(avg_mat)
 plot(mat_pcs$sdev^2 / sum(mat_pcs$sdev^2), type='o', ylab="Pct Variance", ylim=c(0, 1))
 
-jitter_amt = 0.00
-png("plot.png", width=851, height=755)
-plot(mat_pcs$x[, 1], mat_pcs$x[, 2], col="lightblue", cex=0.7, pch=19)
-# text(jitter(mat_pcs$x[, 2], amount=jitter_amt) ~ jitter(mat_pcs$x[, 1], amount=jitter_amt), labels = name_vec, cex=0.8)
-text(mat_pcs$x[, 2] ~ mat_pcs$x[, 1], labels = name_vec, cex=0.8)
-dev.off()
+# jitter_amt = 0.00
+# png("plot.png", width=851, height=755)
+# plot(mat_pcs$x[, 1], mat_pcs$x[, 2], col="lightblue", cex=0.7, pch=19)
+# # text(jitter(mat_pcs$x[, 2], amount=jitter_amt) ~ jitter(mat_pcs$x[, 1], amount=jitter_amt), labels = name_vec, cex=0.8)
+# text(mat_pcs$x[, 2] ~ mat_pcs$x[, 1], labels = name_vec, cex=0.8)
+# dev.off()
 
 
 distance_mat <- matrix(NA, nrow=nrow(avg_mat), ncol=nrow(avg_mat))

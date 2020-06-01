7e37e661523b45d1d55453020e0b1e6f9ae2c307
Ali Turfah <aturfah@gmail.com>
2020-05-31 23:07:55 -0400

    Rearrange pipeline

--- run_pipeline.sh
+++ run_pipeline.sh
@@ -6,14 +6,17 @@ python pull_data.py
 echo "Generating teams..."
 python generate_teams.py
 
+echo "Fitting vector embeddings"
+python fit_model.py
+
+echo "Postprocessing"
+python postprocess.py
+
 echo "Running KL Divergence Sanity Checks"
 echo "::Marginal"
 Rscript sanity\ checks/marginal_sanity_check.R 
 echo "::Conditional"
 Rscript sanity\ checks/cond_sanity_check.R 
 
-echo "Fitting vector embeddings"
-python fit_model.py
-
-echo "Postprocessing"
-python postprocess.py
+echo "Getting Vector Clusters"
+Rscript analysis.R

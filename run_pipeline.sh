0b8428d9c98334776dfbf3acf558d71f32fe6bb8
Ali Turfah <aturfah@gmail.com>
2020-05-31 22:37:19 -0400

    Run sanity checks for marginal and conditional

--- run_pipeline.sh
+++ run_pipeline.sh
@@ -6,6 +6,12 @@ python pull_data.py
 echo "Generating teams..."
 python generate_teams.py
 
+echo "Running KL Divergence Sanity Checks"
+echo "::Marginal"
+Rscript sanity\ checks/marginal_sanity_check.R 
+echo "::Conditional"
+Rscript sanity\ checks/cond_sanity_check.R 
+
 echo "Fitting vector embeddings"
 python fit_model.py
 
